e0f3d8cde391fc1a9c0ab040b2a66b52c74bb734
benjaminbyan <70220796+benjaminbyan@users.noreply.github.com>
2021-02-04 16:12:21 -0600

    Create printing.java

---
+++ printing.java
@@ -0,0 +1,13 @@
+//note that the name of the file matches the public class name
+
+public class printing{
+
+	public static void main(String[] args){
+
+		//two print statements
+		System.out.println("Hello!");
+		System.out.println("Welcome to AP Computer Science A");
+
+	}
+
+}
