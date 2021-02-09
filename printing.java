40d1da9b0772452998548d10f131e1bc2d3e6808
benjaminbyan <70220796+benjaminbyan@users.noreply.github.com>
2021-02-08 21:10:51 -0600

    Update printing.java

--- printing.java
+++ printing.java
@@ -1,13 +1,11 @@
 //note that the name of the file matches the public class name
+//a program for welcoming you to AP CSA via the print commands
 
-public class printing{
-
-	public static void main(String[] args){
-
-		//two print statements
+public class printing
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello!");
 		System.out.println("Welcome to AP Computer Science A");
-
 	}
-
 }
